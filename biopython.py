d7d923346d9a45956b7cbf635bb9e44a9bcbf0d7
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-22 12:57:11 +0100

    wrote function to find max gc content of a sequence

--- biopython.py
+++ biopython.py
@@ -1,4 +1,18 @@
 from Bio import SeqIO
 
-for record in SeqIO.parse("gene.fna", "fasta"):
-    print(record.id)
+def find_max_gc_content(file_name, file_format):
+    max_sequence_id = None
+    max_gc_content = 0
+
+    for record in SeqIO.parse(file_name, file_format):
+        sequence = str(record.seq)
+        sequence_id = record.id
+        gc_content = (sequence.count("G") + sequence.count("C")) / len(sequence)
+        if gc_content > max_gc_content:
+            max_sequence_id = sequence_id
+            max_gc_content = gc_content
+
+    print(max_sequence_id)
+    print(max_gc_content)
+
+find_max_gc_content("gene.fna", "fasta")
